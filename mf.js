6cd35fdc329d439115fc24c66210f94d988ad837
WebWiz34 <100660756+WebWiz34@users.noreply.github.com>
2022-05-13 10:06:02 -0700

    Update mf.js

--- mf.js
+++ mf.js
@@ -1,12 +1,19 @@
+
+
 var rectWidth = 350;
 var rectHeight = 300;
-var startingLine = 10;
+var startingLine = 100;
 
+/* playing with number is as easy as assigning a variable. */
+
+
+/* Then we will assign some math to the variables and use it in our program */
 
 var x = 50 + 50;
 var y = startingLine + 350;
 var z = 400 * 2;
 var finishLine = (rectHeight/ 2) + 1500; 
+
 
 
 var canvas = document.getElementById('creativeCoding');
@@ -34,13 +41,24 @@ function draw() {
    
     fix_dpi(); 
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'rgb(255, 255, 0)';
+    ctx.fillStyle = 'rgb(0, 255, 0)'; 
 
 
 /* now we will pass the variable */
 
-
+   
     ctx.fillRect(startingLine, x, rectWidth, rectHeight);
     ctx.fillRect(startingLine, y, rectWidth, rectHeight);
     ctx.fillRect(startingLine, z, rectWidth, rectHeight);
-    ctx.fillRect(finishLine, 40, 10, 1500);
+    ctx.fillRect(finishLine, 0, 20, 1500);
+
+
+    }
+  }
+
+/* go back to your variables and play around with the numbers
+   you will see how variables can help with speeding up iteration
+   and modification */
+
+
+
